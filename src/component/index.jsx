28aa8518b726682845d7d8661e35d8b1b06a39aa
Godwin <immanuvelgodwin.d@primesoftinc.com>
2025-05-22 11:25:51 +0530

    Overall designs has been completed for the container 4 where 4 different color boxes has been implemented

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -2,15 +2,23 @@ import './index.css'
 import { Box, Typography } from "@mui/material"
 import Grid from '@mui/material/Grid'
 import Header from "../header/header"
-import Slide1 from '../assets/slide1.png';
-import Slide2 from '../assets/slide2.png';
-import Slide3 from '../assets/slide3.png';
+import Slide1 from '../assets/sliders/slide1.png';
+import Slide2 from '../assets/sliders/slide2.png';
+import Slide3 from '../assets/sliders/slide3.png';
 import GirlPic from '../assets/girl.png';
-import Arrow from '../assets/arrow-right.png';
 import GroupIcon from '../assets/svgcomponents/GroupIcon';
 import QualitativeIcon from '../assets/svgcomponents/QualitativeIcon'
 import QuantitativeIcon from '../assets/svgcomponents/QuantitativeIcon'
 import ArrowRight from '../assets/svgcomponents/Arrow';
+import GreenBG from '../assets/summary/Green square.png'
+import GreenBGOverlay from '../assets/summary/Green square 2.png'
+import VioletBG from '../assets/summary/violet rectangle.png'
+import VioletBGOverlay from '../assets/summary/violet rectangle 2.png'
+import GreyBG from '../assets/summary/grey rectangle.png'
+import GreyBGOverlay from '../assets/summary/grey rectangle 2.png'
+import RedBG from '../assets/summary/red square.png'
+import RedBGOverlay from '../assets/summary/red square 2.png'
+import RedBGOverlay2 from '../assets/summary/red square 3.png'
 
 const Home = () => {
 
@@ -106,8 +114,41 @@ const Home = () => {
             </Grid>
 
             {/* container 4 */}
-            <Grid>
+            <Grid className='container-4' container rowSpacing={2}>
+                <Grid container size={{ xs: 12, md: 9, xl: 6 }}>
+                    <Typography className='summary-title'>We’re a full-service B2B market research thought partner committed to strengthening your strategies with deep, subject matter expertise.</Typography>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <div className='image-container'>
+                            <img src={GreenBG} className='image' />
+                            <img src={GreenBGOverlay} className='sub-image' />
+                        </div>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 8 }}>
+                        <div className='image-container'>
+                            <img src={VioletBG} className='image' />
+                            <img src={VioletBGOverlay} className='sub-image' />
+                        </div>
+                    </Grid>
+                </Grid>
 
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 12, md: 8 }}>
+                        <div className='image-container'>
+                            <img src={GreyBG} className='image' />
+                            <img src={GreyBGOverlay} className='sub-image' />
+                        </div>
+                    </Grid>
+
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <div className='image-container'>
+                            <img src={RedBG} className='image' />
+                            <img src={RedBGOverlay} className='sub-image-2' />
+                            <img src={RedBGOverlay2} className='sub-image' style={{ paddingBottom: '10px', paddingRight: '40px' }} />
+                        </div>
+                    </Grid>
+                </Grid>
             </Grid>
         </Box>
     )
